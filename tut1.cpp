a597579f444b360b89ea267453c79c92f5a0b762
MuskanPhogat <muskanphogat024@gmail.com>
2024-09-17 09:28:01 +0530

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,25 @@
+/* HIGH CODE READABILTY is always preffered*/
+//int main() {} this is actually a function
+//iostream is header file for input output
+#include<iostream>
+
+
+int main() {
+    int a=3, b=6;
+    std::cout<<"hello world"<< a << b;
+    return 0;
+}
+// variable=container to hold data
+/*five types:
+int (integer number)
+float(decimal) low precision
+boolean(true or false)
+char(1 byte only one character is stored )
+double(big number or decimal number of high precision)
+datatype variablename=value;*/
+//scope=>local scope
+//global scope
+//user defined datatype
+ /*union
+ enum
+ struct*/
---
+++ tut3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+// << this is insertion operator 
+//  >> this is extraction operator
+int main() {
+    int num1,num2;
+cout<<"the value of num1 is ";
+cin>>num1;
+cout<<"the value of num2 is ";
+cin>>num2;
+cout<<"the sum is " <<num1+num2;
+}
